af618221763b8c49a350c2a4f48c40e312087b6f
Ryan Lee <Renaskigi@protonmail.com>
2018-05-03 15:00:15 -0700

    added constructor functionality

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+function Project (name, description) {
+    this.name = name;
+    this.description = description;
+}
+
+var RPG = new Project ('Dungeon Fellows', 'Dungeon Fellows is a collaborative project that I took part in building with two other students over a period of three and a half days. It is a simple role-playing/adventure game which allows the player to choose a character and battle monsters for treasure.')
