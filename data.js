bea32bd8275cc96d4c4e0fb0563b879731f93475
Ryan Lee <Renaskigi@protonmail.com>
2018-05-09 18:52:48 -0700

    added template to HTML, some functions to change classes to app.js, separated data into data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const rawData = [
+    {
+        name:'Dungeon Fellows',
+        completed: 'February 2018',
+        description:'Dungeon Fellows is a collaborative project that I took part in building with two other students over a period of three and a half days. It is a simple role-playing/adventure game which allows the player to choose a character and battle monsters for treasure.',
+        photo:'<img class="pixpage1" src="/pix/dungeonfellows.png" alt="pic1"><br>',
+        link:'<a = href="https://4ampersands.github.io/rpg/">Show Me Dungeon Fellows!</a>'
+    },
+    {
+        name:'Ipsum Project',
+        completed: 'tbd',
+        description:'ancient alien ufo ancient alien pre-colonial aerodynamics mainstream archaelogy sanskrit, targeted mutation vymaanika-shaastra sightings choral castle cover up, ufo kachina doll sun disc . the answer is a resounding yes... Electromagnetic burmuta triangle mahabharata cover up spaceships mahabharata, space brothers ezekiel petroglyph aircraft vimana ancient alien ancient god , extraterrestrial origin golden disk space brothers sightings ancient religions sightings. star people sumerian texts foo fighter astronaut petroglyph inter-dimensional golden disk, technology sightings anti-gravity weightless mainstream archaelogy easter island nasa. golden disk sightings space brothers choral castle legendary times, seti worm hole space travel chariot of the gods, seti contend earth mound cover up .'
+    },
+
+];
